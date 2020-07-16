b181e2b024c70405063154b2805c4da36819540d
melnik <kivals.90@yandex.ru>
2020-07-16 13:38:25 +0300

    add vdomkota

---
+++ vdomkota/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function () {
+  ibg(); // $('.intro-slider').slick({
+  // 	arrows: false,
+  // 	dots: true,
+  // 	speed: 1000,
+  // 	autoplay: true,
+  // 	autoplaySpeed: 8000
+  // });
+
+  $('.slider-cats').slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
+
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', "url(\"".concat($(this).find('img').attr('src'), "\")"));
+    }
+  });
+}
